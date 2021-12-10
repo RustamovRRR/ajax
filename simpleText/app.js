893d6d86998b83e7fd1b46fce669647212a9e197
RustamovRRR <rustamrrustamov@gmail.com>
2021-12-10 23:26:22 +0400

    understanding XMLHttpRequest and his methods and properties

--- simpleText/app.js
+++ simpleText/app.js
@@ -1,8 +1,13 @@
+// XMLHttpRequest (XHR) objects are used to interact with servers. You can retrieve data from a URL without having to do a full page refresh.
 const xhr = new XMLHttpRequest();
 console.log(xhr);
 
+// The XMLHttpRequest method open() initializes a newly-created request, or re-initializes an existing one.
 xhr.open("GET", "./api/sample.txt");
+// onreadystatechange - An event handler that is called whenever the readyState attribute changes.
 xhr.onreadystatechange = function () {
+  // The XMLHttpRequest.readyState property returns the state an XMLHttpRequest client is in
+  // The read-only XMLHttpRequest.status property returns the numerical HTTP status code of the XMLHttpRequest's response.
   if (xhr.readyState === 4 && xhr.status === 200) {
     const text = document.createElement("p");
     text.textContent = xhr.responseText;
@@ -11,7 +16,9 @@ xhr.onreadystatechange = function () {
     console.log({
       status: xhr.status,
       statusText: xhr.statusText,
+      state: xhr.readyState,
     });
   }
 };
+// The XMLHttpRequest method send() sends the request to the server.
 xhr.send();
