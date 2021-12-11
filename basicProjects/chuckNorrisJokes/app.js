e5dcff6207bb48c523a52e1ac10888a3c6cf7686
RustamovRRR <rustamrrustamov@gmail.com>
2021-12-11 23:52:44 +0400

    defining btn and container

--- basicProjects/chuckNorrisJokes/app.js
+++ basicProjects/chuckNorrisJokes/app.js
@@ -1,1 +1,6 @@
 const url = "https://api.chucknorris.io/jokes/random";
+
+const btn = document.querySelector(".btn");
+const content = document.querySelector(".content");
+
+btn.addEventListener("click", () => {});
