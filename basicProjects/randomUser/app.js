fe4763a19b278d50bbeea24db496387c391a5cc0
RustamovRRR <rustamrrustamov@gmail.com>
2021-12-12 23:07:05 +0400

    creating some modules for finishing the project

--- basicProjects/randomUser/app.js
+++ basicProjects/randomUser/app.js
@@ -1,18 +1,17 @@
 import getElement from "./modules/getElement.js";
 import getUser from "./modules/fetchUser.js";
+import displayUser from "./modules/displayUser.js";
 
-const img = getElement(".user-img");
-const title = getElement(".user-title");
-const value = getElement(".user-value");
 const btn = getElement(".btn");
 
-const btns = [...document.querySelectorAll(".icon")];
+
 
 const showUser = async () => {
   // get user from api
   const person = await getUser();
-  console.log(person);
+
   // display user
+  displayUser(person);
 };
 
 window.addEventListener("DOMContentLoaded", showUser);
