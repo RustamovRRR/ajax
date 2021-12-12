ce8342f05cbc42eff58e20436467f3b32a54fa82
RustamovRRR <rustamrrustamov@gmail.com>
2021-12-12 15:35:30 +0400

    getting data from api and assign it to the content

--- basicProjects/chuckNorrisJokes/app.js
+++ basicProjects/chuckNorrisJokes/app.js
@@ -1,6 +1,21 @@
-const url = "https://api.chucknorris.io/jokes/random";
+const URL = "https://api.chucknorris.io/jokes/random";
 
 const btn = document.querySelector(".btn");
 const content = document.querySelector(".content");
 
-btn.addEventListener("click", () => {});
+btn.addEventListener("click", () => {
+  getData(URL);
+});
+
+function getData(url) {
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", url);
+  xhr.send();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState !== 4) return;
+    if (xhr.status === 200) {
+      const { value: joke } = JSON.parse(xhr.responseText);
+      content.textContent = joke;
+    }
+  };
+}
