34a5172c8520960c1fd958907ab2064129a90934
RustamovRRR <rustamrrustamov@gmail.com>
2021-12-12 16:37:32 +0400

    understanding and using fetch

--- basicProjects/chuckNorrisJokes/app.js
+++ basicProjects/chuckNorrisJokes/app.js
@@ -5,30 +5,15 @@ const content = document.querySelector(".content");
 const img = document.querySelector(".container img");
 
 btn.addEventListener("click", () => {
-  getData(urlAPI)
+  fetch(urlAPI)
+    .then((data) => data.json())
     .then((response) => displayData(response))
     .catch((err) => console.log(err));
 });
 
-function getData(url) {
-  return new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", url);
-    xhr.send();
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState !== 4) return;
-      if (xhr.status === 200) {
-        resolve(xhr.responseText);
-      } else {
-        reject({ status: xhr.status, text: xhr.statusText });
-      }
-    };
-  });
-}
-
-function displayData(data) {
+function displayData({ value: joke }) {
   img.classList.add("shake-img");
-  const { value: joke } = JSON.parse(data);
+  //   const { value: joke } = JSON.parse(data);
   content.textContent = joke;
   const random = Math.random() * 1000;
   setTimeout(() => {
